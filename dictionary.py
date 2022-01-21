26fcfd7596f343c10f24ece12d045b2b9333a923
HP2020SMU <73238098+HP2020SMU@users.noreply.github.com>
2022-01-21 09:16:32 +0800

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+## 字典dictionary
+# key， value
+# 键，  值
+# 如何理解： 查字典时，（输入）被查询的字 可视为 键， （输出）注释的内容就是 值
+
+# 如何创建一个字典：
+# dic = {"苹果": "apple", "香蕉": "banana", "猫": "cat", "狗": "dog"}
+# print(dic["苹果"])
+# print(dic["猫"])
+dic = {0: "apple", 1: "banana", 2: "cat", 3: "dog"}
+print(dic[0])
+print(dic[3])
